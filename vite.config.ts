4188887c38a2bf569b883f8b3a5444ec69f377a1
moesu01 <moe@kydlabs.com>
2025-09-22 02:52:01 -0400

    setting up for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kyd-lending-pool/',
   plugins: [
     react(),
     tailwindcss()
